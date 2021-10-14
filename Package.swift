e853689a0e920bfcb779605d1a3914fb5acd7f00
Rake Yang <fenglaijun@gmail.com>
2021-10-14 14:28:19 +0800

    修改依赖

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/BinaryParadise/PracticeTLS.git", from: "1.0.5")
+        .package(url: "https://github.com/BinaryParadise/PracticeTLS.git", .upToNextMinor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
